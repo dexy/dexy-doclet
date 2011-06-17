7962f90af2b7b2c6d098769f6c16b3234a8d55fa
Ana Nelson <ana@ananelson.com>
2011-06-17 16:46:07 +0100

    Tidy up ant tasks. Have an example task which runs standard doclet and jsondoclet.

--- tests/unit/it/dexy/jsondoclet/DocletTest.java
+++ tests/unit/it/dexy/jsondoclet/DocletTest.java
@@ -5,17 +5,4 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DocletTest {
-
-  @Test
-  public void countLines() {
-    String empty = "";
-    String one_line = "abc";
-    String two_lines = "abc\ndef";
-
-    Doclet doclet = new Doclet();
-
-    assertTrue(doclet.countLines(empty)==0);
-    assertTrue(doclet.countLines(one_line)==1);
-    assertTrue(doclet.countLines(two_lines)==2);
-  }
 }
